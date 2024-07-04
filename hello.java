822c6c26fcf499950fdeba7afa0fb1b140472ad9
minhyeok0312 <152257964+minhyeok0312@users.noreply.github.com>
2024-07-04 13:29:38 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,23 +1,19 @@
-// 1. package(패키지) 선언
-package com.company;
-
-// 2. import(참조) 선언
+// 1. import(참조) 선언
 import java.lang.String;
 
-// 3. 클래스 선언
+// 2. 클래스 선언
 public class Member {
 
-    // 4. main 메서드 선언
+    // 3. main 메서드 선언
     public static void main(String[] args) {
         String message = hello(30, "1번 회원");
         System.out.println(message);
         System.out.println(hello(27, "2번 회원"));
     }
 
-    // 5. hello 메서드 선언
+    // 4. hello 메서드 선언
     public static String hello(int age, String name) {
         return age + "살" + name + " 님 만나서 반갑습니다!";
     }
 
 }
-출처: https://congsong.tistory.com/65 [Let's develop:티스토리]
